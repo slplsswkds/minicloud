dcee1471528af21ae4e33effffb37c5e982bff0c
slplsswkds <slplsswkds@protonmail.com>
2023-06-28 07:34:04 +0300

    some refactoring

---
+++ src/server.rs
@@ -0,0 +1,55 @@
+use std::{
+    path::{PathBuf},
+};
+use axum::{
+    body::StreamBody,
+    http::{header, StatusCode},
+    response::{Html, IntoResponse}, 
+    extract::{State, Query}, 
+};
+use serde::Deserialize;
+use tokio_util::io::ReaderStream;
+
+pub async fn root_handler(State(files): State<Vec<PathBuf>>) -> Html<String> {
+    let mut list = String::new();
+    list += "<p1> ";
+    for file in files.into_iter() {
+        list += &format!("<a href=\"{}\">{}</a> </br> ", "/download?name=".to_string() + &file.to_str().unwrap(),file.to_str().unwrap());
+    }
+    list += " <p1>";
+    return Html(list);
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Filename {
+    name: Option<String>
+}
+
+pub async fn download_file(Query(filename): Query<Filename>) -> impl IntoResponse {
+    let filepath = match filename.name {
+        Some(path) => PathBuf::from(&path) ,
+        None => return Err((StatusCode::NOT_FOUND, format!("Missing file name"))),
+    };
+
+    println!("filepath = {}", &filepath.to_str().unwrap());
+
+    let content_type = match mime_guess::from_path(&filepath).first_raw() {
+        Some(mime) => mime,
+        None => return Err((StatusCode::BAD_REQUEST, "MIME Type couldn't be determined".to_string()))
+    };
+
+    let file = match tokio::fs::File::open(&filepath).await {
+        Ok(file) => file,
+        Err(err) => return Err((StatusCode::NOT_FOUND, format!("File not found: {}", err))),
+    };
+    let stream = ReaderStream::new(file);
+    let body = StreamBody::new(stream);
+    //let name = &format!("attachment; filename=\"{:?}\"", filepath.file_name());
+    let headers = [
+        (header::CONTENT_TYPE, content_type),
+
+        //(header::CONTENT_DISPOSITION, "attachment; filename=\"file.mp3\""), // To force file download
+    ];
+
+    Ok((headers, body))
+}
---
+++ src/storage.rs
@@ -0,0 +1,31 @@
+use std::{
+    fs::canonicalize,
+    path::{PathBuf},
+};
+use colored::Colorize;
+
+pub fn parse_paths(str_path: &Vec<String>) -> Vec<PathBuf> {
+    let mut files = Vec::new();
+    
+    for arg in str_path {
+        let path = PathBuf::from(&arg);
+        if path.exists() {
+            let full_path = canonicalize(&path).unwrap();
+            files.push(full_path);
+        }
+        else if path.is_dir() {
+            println!("\"{}\" is directory. This is not yet supported. Ignoring the argument...", arg.yellow());
+        }
+        else {
+            println!("File \"{}\" not found. Ignoring the argument...", arg.yellow());
+        }
+    }
+    files.dedup();
+    return files;
+}
+
+pub fn files_show(files: &Vec<PathBuf>) {
+    for file in files.into_iter() {
+        println!("{}", file.to_str().unwrap().cyan());
+    }
+}
