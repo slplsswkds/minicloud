4115c8a6343c4249161b026d3bcfd44afb3a5137
slplsswkds <slplsswkds@protonmail.com>
2024-08-29 02:36:04 +0300

    Downloading files

--- src/server.rs
+++ src/server.rs
@@ -2,10 +2,12 @@ use std::{collections::HashMap, sync::Arc};
 use axum::{
     extract::{State, Query},
     response::{Html, IntoResponse},
-    http::StatusCode,
+    http::{StatusCode, HeaderMap, header},
+    body::Body,
 };
 use crate::fs_object::FSObject;
 use serde::Deserialize;
+use tokio_util::io::ReaderStream;
 
 pub async fn root_handler(page: State<Arc<Html<String>>>) -> impl IntoResponse {
     (**page).clone()
@@ -27,7 +29,27 @@ pub async fn download_handler(
             return Err((StatusCode::NOT_FOUND, err_msg));
         }
     };
-    Ok(Html(format!("required item: {}", fs_object.name()).to_string()))
+    // Ok(Html(format!("required item: {}", fs_object.name()).to_string()));
+
+    // `File` implements `AsyncRead`
+    let file = match tokio::fs::File::open(&fs_object.path).await {
+        Ok(file) => file,
+        Err(err) => return Err((StatusCode::NOT_FOUND, format!("File not found: {}", err))),
+    };
+
+    // Convert the `AsyncRead` into a `Stream`
+    let stream = ReaderStream::new(file);
+
+    // Convert the `Stream` into an `axum::body::HttpBody`
+    let body = Body::from_stream(stream);
+
+    // Create headers for the response
+    let mut headers = HeaderMap::new();
+    headers.insert(header::CONTENT_TYPE, "text/toml; charset=utf-8".parse().unwrap());
+    headers.insert(header::CONTENT_DISPOSITION, "attachment; filename=\"Cargo.toml\"".parse().unwrap());
+
+    // Combine headers and body into a response
+    Ok((StatusCode::OK, headers, body))
 }
 
 // pub async fn root_handler(State(files): State<Vec<PathBuf>>) -> Html<String> {
@@ -67,20 +89,20 @@ pub async fn download_handler(
 
 //     Ok((headers, body))
 // }
-
+//
 // pub async fn preview_file(Query(filename): Query<Filename>) -> impl IntoResponse {
 //     let filepath = match filename.name {
 //         Some(path) => PathBuf::from(&path) ,
 //         None => return Err((StatusCode::NOT_FOUND, format!("Missing file name"))),
 //     };
-
+//
 //     println!("filepath = {}", &filepath.to_str().unwrap());
-
+//
 //     let content_type = match mime_guess::from_path(&filepath).first_raw() {
 //         Some(mime) => mime,
 //         None => return Err((StatusCode::BAD_REQUEST, "MIME Type couldn't be determined".to_string()))
 //     };
-
+//
 //     let file = match tokio::fs::File::open(&filepath).await {
 //         Ok(file) => file,
 //         Err(err) => return Err((StatusCode::NOT_FOUND, format!("File not found: {}", err))),
@@ -88,6 +110,6 @@ pub async fn download_handler(
 //     let stream = ReaderStream::new(file);
 //     let body = StreamBody::new(stream);
 //     let headers = [(header::CONTENT_TYPE, content_type)];
-
+//
 //     Ok((headers, body))
 // }
