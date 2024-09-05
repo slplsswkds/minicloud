982a00771ef966bd4d47ada4d393ce34802bc900
slplsswkds <slplsswkds@protonmail.com>
2024-09-05 04:53:43 +0300

    cli args parser fixes

--- src/cli_args.rs
+++ src/cli_args.rs
@@ -1,5 +1,3 @@
-//! Еhis file contains elements for storing and preparing data entered as command line arguments
-
 use clap::Parser;
 use std::path::PathBuf;
 
@@ -9,7 +7,6 @@ use std::path::PathBuf;
 #[command(author, version, about, long_about = None)]
 pub struct Args {
     /// Set directories and files that will be distributed
-    #[arg(value_delimiter = ',')]
     pub paths: Vec<PathBuf>,
 
     /// Port number
@@ -24,48 +21,56 @@ pub struct Args {
 impl Args {
     /// Prepare vector of paths for using. Remove wrong paths and print errors.
     pub fn prepare_data(&mut self) -> Result<(), ()> {
-        self.optimize_paths();
-
-        let validation = self.validate_paths();
-
-        if let Err(errors) = validation {
-            errors
-                .iter()
-                .for_each(|wrong_path| println!("Error: Path {:?} is not exist", wrong_path));
-            return Err(()); // Close minicloud
-        }
-
         if self.canonical {
             self.canonicalize_paths();
+        } else {
+            self.remove_inaccessible_paths()
+                .iter()
+                .for_each(|wrong_path| {
+                    eprintln!("Error: Path {:?} does not exist", wrong_path);
+                });
         }
+
+        self.remove_repeated_paths();
 
         Ok(())
     }
 
-    /// Removes consecutive repeated elements in the vector
-    fn optimize_paths(&mut self) {
+    /// Removes repeated elements in the vector
+    fn remove_repeated_paths(&mut self) {
+        self.paths.sort();
         self.paths.dedup();
     }
 
-    /// Returns vector of paths that do not exist in the file system
-    fn validate_paths(&self) -> Result<(), Vec<PathBuf>> {
-        let errors: Vec<PathBuf> = self
-            .paths
-            .iter()
-            .filter(|path| !path.exists())
-            .cloned()
-            .collect();
+    /// Returns a vector of paths that do not exist in the file system or cannot be accessed.
+    ///
+    /// This function will traverse the symbolic links to get information about the target file.
+    /// If you cannot access the file's metadata, e.g. g. due to a permission error or faulty symlinks,
+    /// this will copy the path into a vector of wrong paths
+    fn remove_inaccessible_paths(&mut self) -> Vec<PathBuf> {
+        let mut wrong_paths = Vec::new();
 
-        if !errors.is_empty() {
-            return Err(errors);
-        }
-        Ok(())
+        self
+            .paths
+            .retain(|path| {
+                if path.exists() {
+                    true
+                } else {
+                    wrong_paths.push(path.clone());
+                    false
+                }
+            });
+
+        wrong_paths
     }
 
-    /// canonicalize all pathes
+    /// Canonicalize all paths
     fn canonicalize_paths(&mut self) {
         for path in self.paths.iter_mut() {
-            *path = path.canonicalize().unwrap();
+            match path.canonicalize() {
+                Ok(canonical_path) => *path = canonical_path,
+                Err(e) => eprintln!("Warning: Could not canonicalize path {:?}: {}", path, e),
+            }
         }
     }
 }
