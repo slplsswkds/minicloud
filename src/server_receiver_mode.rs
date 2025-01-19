e7d21df7ba5ee6a3bc1c58bb20e4c68e7ed62249
slplsswkds <slplsswkds@protonmail.com>
2025-01-19 14:14:38 +0200

    error handling

--- src/server_receiver_mode.rs
+++ src/server_receiver_mode.rs
@@ -1,14 +1,14 @@
 use crate::cli_args::Args;
 use axum::extract::{DefaultBodyLimit, Multipart, State};
+use axum::http::StatusCode;
 use axum::response::{Html, IntoResponse};
 use axum::routing::get;
 use axum::Router;
-use axum::http::StatusCode;
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
 use tokio::io::AsyncWriteExt;
 use tower_http::limit::RequestBodyLimitLayer;
-use tracing::{error, info};
+use tracing::{error, info, warn};
 
 pub fn setup(cli_args: &Args) -> Router {
     let uploads_path_state = Arc::new(cli_args.received_files_path.clone());
@@ -28,6 +28,7 @@ pub fn setup(cli_args: &Args) -> Router {
 }
 
 pub async fn show_upload_form() -> Html<&'static str> {
+    info!("Root page request");
     Html(
         r#"
         <!DOCTYPE html>
@@ -49,7 +50,6 @@ pub async fn show_upload_form() -> Html<&'static str> {
     )
 }
 
-// REMOVE UNWRAPS!!!!!!!!!!!!
 pub async fn accept_upload_form(
     uploads_path_state: State<Arc<PathBuf>>,
     mut multipart: Multipart,
@@ -57,19 +57,41 @@ pub async fn accept_upload_form(
     let create_dir = tokio::fs::create_dir_all(uploads_path_state.as_ref());
 
     if create_dir.await.is_err() {
-        error!("Failed to create directory {}", uploads_path_state.as_ref().display());
+        error!(
+            "Failed to create directory {}",
+            uploads_path_state.as_ref().display()
+        );
         return Err(StatusCode::INTERNAL_SERVER_ERROR.into_response());
     }
 
-    while let Some(field) = multipart.next_field().await.unwrap() {
-        let name = field.name().unwrap().to_string();
-        let file_name = field.file_name().unwrap().to_string();
-        let content_type = field.content_type().unwrap().to_string();
-        let data = field.bytes().await.unwrap();
+    while let Some(field) = multipart
+        .next_field()
+        .await
+        .map_err(IntoResponse::into_response)?
+    {
+        let name = field.name().unwrap_or("unnamed").to_string();
+        let file_name = field.file_name().unwrap_or("unnamed").to_string();
+        let content_type = field.content_type().unwrap_or("unknown-type").to_string();
+        let data = field.bytes().await.map_err(|err| {
+            warn!("Failed to read multipart data: {}", err);
+            err.into_response()
+        })?;
 
         let file_path = uploads_path_state.as_ref().clone().join(file_name);
-        let mut file = tokio::fs::File::create(file_path).await.unwrap();
-        file.write_all(&data).await.unwrap();
+
+        let mut file = tokio::fs::File::create(file_path.clone())
+            .await
+            .map_err(|err| {
+                error!("Failed to create file: {}", err);
+                StatusCode::INTERNAL_SERVER_ERROR.into_response()
+            })?;
+
+        file.write_all(&data).await.map_err(|err| {
+            error!("Failed to save file: {}", err);
+            StatusCode::INTERNAL_SERVER_ERROR.into_response()
+        })?;
+
+        info!("Received file: {}", file_path.display());
     }
 
     Ok("Upload successful".into_response())
